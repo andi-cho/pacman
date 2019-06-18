b44f6f8f9cf699199268f04567cbb6e1d08c6742
Amir Farhat <amirf@mit.edu>
2019-06-17 21:46:49 -0400

    We have a moving stick figure yay

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ CLOCK = pygame.time.Clock()
 # ---------------------------------------- PRE-GAME SETUP
 
 # init the game
-game.init()
+pygame.init()
 
 # make game screen dimensions according to setup
-screen = game.display.set_mode([WIDTH, HEIGHT])
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
 # set game caption to specifed value
 pygame.display.set_caption(GAME_TITLE)
@@ -46,6 +46,24 @@ x_coord = 10
 y_coord = 10
 
 
+# ---------------------------------------- HELPERS
+
+def draw_stick_figure(screen, x, y):
+    # head
+    pygame.draw.ellipse(screen, BLACK, [1 + x, y, 10, 10], 0)
+ 
+    # legs
+    pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [10 + x, 27 + y], 2)
+    pygame.draw.line(screen, BLACK, [5 + x, 17 + y], [x, 27 + y], 2)
+ 
+    # body
+    pygame.draw.line(screen, RED, [5 + x, 17 + y], [5 + x, 7 + y], 2)
+ 
+    # arms
+    pygame.draw.line(screen, RED, [5 + x, 7 + y], [9 + x, 17 + y], 2)
+    pygame.draw.line(screen, RED, [5 + x, 7 + y], [1 + x, 17 + y], 2)
+
+
 # ---------------------------------------- MAIN
 
 done = False
@@ -53,12 +71,39 @@ done = False
 while not done:
     # process events
     for event in pygame.event.get():
+
+    	# user presses the X button
         if event.type == pygame.QUIT:
             done = True
             break
+
+        # user pressed an arrow key on keyboard
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+
+        # user releases key
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_speed = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_speed = 0
 	
 	# draw background color
     screen.fill(WHITE)
+
+    # move person
+    x_coord += x_speed
+    y_coord += y_speed
+
+    # draw
+    draw_stick_figure(screen, x_coord, y_coord)
  
     # update the screen with what we've drawn.
     pygame.display.flip()
