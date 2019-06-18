05aebae2a1c8c2ab4d803e5d6f5c3a31d38caf76
Amir Farhat <amirf@mit.edu>
2019-06-17 21:14:57 -0400

    Update the file structure

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-import pygame
+# ---------------------------------------- IMPORTS
 
+import pygame
 from player import Player
 
+# ---------------------------------------- CONSTANTS & GLOBALS
+
+
+# ---------------------------------------- PRE-GAME SETUP
+
+pygame.init()
 p = Player()
+
+
+# ---------------------------------------- MAIN
+
+pygame.quit()
