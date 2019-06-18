02d606e888c63a823e7ffbefa4307b7963dfe142
Amir Farhat <amirf@mit.edu>
2019-06-17 21:37:22 -0400

    Screen background and main loop add

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from player import Player
 
 # ---------------------------------------- CONSTANTS & GLOBALS
 
+# some colors in RGB
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED   = (255, 0, 0)
+
 # dimensions of the screen
 WIDTH  = 700 # pixels
 HEIGHT = 500 # pixels
@@ -14,7 +20,7 @@ HEIGHT = 500 # pixels
 GAME_TITLE = 'Our Pacman Game'
 
 # clock to determine refresh rate
-clock = pygame.time.Clock()
+CLOCK = pygame.time.Clock()
 
 
 # ---------------------------------------- PRE-GAME SETUP
@@ -28,7 +34,37 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 # set game caption to specifed value
 pygame.display.set_caption(GAME_TITLE)
 
+# hide the mouse cursor
+pygame.mouse.set_visible(0)
+
+# speed in pixels per frame
+x_speed = 0
+y_speed = 0
+ 
+# current position
+x_coord = 10
+y_coord = 10
+
 
 # ---------------------------------------- MAIN
 
+done = False
+
+while not done:
+    # process events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            break
+	
+	# draw background color
+    screen.fill(WHITE)
+ 
+    # update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # frames per second
+    CLOCK.tick(60)
+ 
+# quit the game
 pygame.quit()
