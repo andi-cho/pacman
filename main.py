02c59b22fa18af9ffaf57598f01a64839c14d06c
Amir Farhat <amirf@mit.edu>
2019-06-17 21:23:05 -0400

    Bare bones game window launching

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 # ---------------------------------------- IMPORTS
 
 import pygame
@@ -5,11 +6,24 @@ from player import Player
 
 # ---------------------------------------- CONSTANTS & GLOBALS
 
+# dimensions of the screen
+WIDTH  = 700 # pixels
+HEIGHT = 500 # pixels
+
+# title of the game
+GAME_TITLE = 'Our Pacman Game'
+
 
 # ---------------------------------------- PRE-GAME SETUP
 
+# init the game
 pygame.init()
-p = Player()
+
+# make game screen dimensions according to setup
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+# set game caption to specifed value
+pygame.display.set_caption(GAME_TITLE)
 
 
 # ---------------------------------------- MAIN
